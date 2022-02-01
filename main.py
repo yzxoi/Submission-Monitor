853e286a26b7061539ad766df27a670ad64886bc
yzxoi <y@yzxoi.top>
2022-02-01 18:45:11 +0800

    add support for luogu

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import requests
 import time
 import json
 from lxml import etree
-from lxml.html import fromstring, tostring
+import urllib.parse
 
 GETFREQ = 300
 MAXCOUNT = 50
 SMAX = 100
 GETTIMEOUT = 20
 GETERRORFREQ = 5
+TimeEquation = 0
+# TimeEquation = 13*3600
+Cookie = "_uid=;__client_id="
 UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
 
 # with open('cookies.json', 'r') as cookiesjson:
@@ -168,6 +171,84 @@ def GetUOJ(now,page):
 		except:
 			print("Error No Such Element!")
 
+def Getluogu(now,page):
+	global cnt, lastGet, MAXCOUNT, UA, GETTIMEOUT, submissions, Cookie
+	lastGet = time.time()
+	while True:
+		try:
+			getheader = {
+				"refer": "https://www.luogu.com.cn",
+				"user-agent": UA,
+				"cookie": Cookie
+			}
+			response = requests.get("https://www.luogu.com.cn/record/list?user=" + str(now['handle']) + "&page=" + str(page), headers = getheader, timeout = GETTIMEOUT,verify=False)
+			selector=etree.HTML(response.text)
+			content=selector.xpath('/html/head/script[1]/text()')[0]
+			content=content[53:len(content)-69]
+			content=urllib.parse.unquote(content)
+			break
+		except:
+			print("Error No Such Page, retrying...")
+			time.sleep(GETERRORFREQ)
+	# try:
+	# print(content)
+	# 12:Accepted, 14: Unaccepted, 2: Compile Error
+	data = json.loads(content)
+	for i in data['currentData']['records']['result']:
+		g_user = []
+		g_user.append(i['user']['uid'])
+		g_user.append(i['user']['name'])
+		g_user.append(i['user']['color'])
+		g_user.append(i['user']['ccfLevel'])
+		submission = []
+		submission.append("Luogu")
+		submission.append(str(i['id']))
+		if(i['status'] == 12):
+			submission.append("Accepted")
+		elif(i['status'] == 14):
+			submission.append("Unaccepted")
+		elif(i['status'] == 2):
+			submission.append("Compile Error")
+		else:
+			submission.append("Unknown Error")
+		try:
+			submission.append(str(i['score']))
+		except:
+			submission.append(str(0))
+		submission.append(str(i['problem']['pid'])+ " " + i['problem']['title'])
+		submission.append("https://www.luogu.com.cn/problem/"+str(i['problem']['pid']))
+		submission.append(g_user)
+		if (i['time']):
+			submission.append(str(i['time'])+" ms")
+		else:
+			submission.append("0 ms")
+		if(i['memory']):
+			submission.append(str(int(i['memory']/1024)) + " MB")
+		else:
+			submission.append("0 MB")
+		go = ''
+		if (i['enableO2'] == True):
+			go = ' O2'
+		if (i['language'] == 12):
+			submission.append("C++ 17" + go)
+		elif (i['language'] == 11):
+			submission.append("C++ 14" + go)
+		elif (i['language'] == 27):
+			submission.append("C++ 20" + go)
+		elif (i['language'] == 4):
+			submission.append("C++ 11" + go)
+		elif (i['language'] == 7):
+			submission.append("Python 3" + go)
+		else:
+			submission.append("Unknown" + go)
+		submission.append("https://www.luogu.com.cn/record/"+str(i['id']))
+		# Because luogu will change the time by your location, so you need to add your time equation
+		submission.append(i['submitTime']+TimeEquation)
+
+		submissions.append(submission)
+	# except:
+		# print("Error No Such Element!")
+
 def GetColor(status):
 	if(status == "Accepted"):
 		return "forestgreen"
@@ -274,6 +355,10 @@ for i in range(0,len(user)):
 		GetUOJ(user[i],1)
 		GetUOJ(user[i],2)
 		GetUOJ(user[i],3)
+	elif (user[i]['type'] == 'luogu'):
+		Getluogu(user[i],1)
+		Getluogu(user[i],2)
+		Getluogu(user[i],3)
 rk = []
 for i in range(0,len(submissions)):
 	rk.append(i)
@@ -299,6 +384,22 @@ with open("index.html", "w", encoding="utf-8") as file:
 			file.write('<td><a style="color:' + GetColor(i[2]) + ';vertical-align: middle;" href="' + i[10] + '"><i class="mdui-icon material-icons" style="height: 26px;">' + GetIconName(i[2]) + '</i> ' + i[2] + '</a></td>')
 			file.write('<td><span style="color:' + GetScoreColor(i[3]) + ';">' + i[3] + '</span></td><td><a style="color:#4183c4;" href="' + i[5] + '">' + i[4] + '</td>')
 			file.write('<td><a style="color:rgb(75, 175, 178);" href="https://uoj.ac/user/profile/' + str(i[6]) + '">' + str(i[6]) + '</a></td>')
+			file.write('<td>' + i[7] + '</td><td>' + i[8] + '</td><td><span>' + i[9] + '</spane></td><td>' + GetTime(i[11]) + '</td>')
+			file.write('</tr>')
+		elif (i[0]=='Luogu'):
+			file.write('<tr>')
+			file.write('<td><a href="https://www.luogu.com.cn/"><img style="height:16px;vertical-align: middle;" src="./luogu.ico" /> 洛谷</a></td>')
+			file.write('<td><a style="color:' + GetColor(i[2]) + ';vertical-align: middle;" href="' + i[10] + '"><i class="mdui-icon material-icons" style="height: 26px;">' + GetIconName(i[2]) + '</i> ' + i[2] + '</a></td>')
+			file.write('<td><span style="color:' + GetScoreColor(i[3]) + ';">' + i[3] + '</span></td><td><a style="color:#4183c4;" href="' + i[5] + '">' + i[4] + '</td>')
+			# uid, name, color, ccfLevel
+			go = ""
+			if (int(i[6][3])>=8):
+				go = ' <svg data-v-303bbf52="" aria-hidden="true" focusable="false" data-prefix="fad" data-icon="badge-check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-badge-check fa-w-16" style="--fa-primary-color:#fff; --fa-secondary-color:#ffc116; --fa-secondary-opacity:1;height: 13px;"><g data-v-303bbf52="" class="fa-group"><path data-v-303bbf52="" fill="currentColor" d="M512 256a88 88 0 0 0-57.1-82.4A88 88 0 0 0 338.4 57.1a88 88 0 0 0-164.8 0A88 88 0 0 0 57.1 173.6a88 88 0 0 0 0 164.8 88 88 0 0 0 116.5 116.5 88 88 0 0 0 164.8 0 88 88 0 0 0 116.5-116.5A88 88 0 0 0 512 256zm-144.8-44.25l-131 130a11 11 0 0 1-15.55-.06l-75.72-76.33a11 11 0 0 1 .06-15.56L171 224a11 11 0 0 1 15.56.06l42.15 42.49 97.2-96.42a11 11 0 0 1 15.55.06l25.82 26a11 11 0 0 1-.08 15.56z" style="color:ffc116"></path><path data-v-303bbf52="" fill="currentColor" d="M367.2 211.75l-131 130a11 11 0 0 1-15.55-.06l-75.72-76.33a11 11 0 0 1 .06-15.56L171 224a11 11 0 0 1 15.56.06l42.15 42.49 97.2-96.42a11 11 0 0 1 15.55.06l25.82 26a11 11 0 0 1-.06 15.56z" style="color:#fff"></path></g></svg>'
+			elif (int(i[6][3])>=6):
+				go = ' <svg data-v-303bbf52="" aria-hidden="true" focusable="false" data-prefix="fad" data-icon="badge-check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-badge-check fa-w-16" style="--fa-primary-color:#fff; --fa-secondary-color:#3498db; --fa-secondary-opacity:1;height: 13px;"><g data-v-303bbf52=""><path data-v-303bbf52="" fill="currentColor" d="M512 256a88 88 0 0 0-57.1-82.4A88 88 0 0 0 338.4 57.1a88 88 0 0 0-164.8 0A88 88 0 0 0 57.1 173.6a88 88 0 0 0 0 164.8 88 88 0 0 0 116.5 116.5 88 88 0 0 0 164.8 0 88 88 0 0 0 116.5-116.5A88 88 0 0 0 512 256zm-144.8-44.25l-131 130a11 11 0 0 1-15.55-.06l-75.72-76.33a11 11 0 0 1 .06-15.56L171 224a11 11 0 0 1 15.56.06l42.15 42.49 97.2-96.42a11 11 0 0 1 15.55.06l25.82 26a11 11 0 0 1-.08 15.56z" style="color:#3498db"></path><path data-v-303bbf52="" fill="currentColor" d="M367.2 211.75l-131 130a11 11 0 0 1-15.55-.06l-75.72-76.33a11 11 0 0 1 .06-15.56L171 224a11 11 0 0 1 15.56.06l42.15 42.49 97.2-96.42a11 11 0 0 1 15.55.06l25.82 26a11 11 0 0 1-.06 15.56z" style="color:#fff"></path></g></svg>'
+			elif (int(i[6][3])>=3):
+				go = ' <svg data-v-303bbf52="" aria-hidden="true" focusable="false" data-prefix="fad" data-icon="badge-check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-badge-check fa-w-16" style="--fa-primary-color:#fff; --fa-secondary-color:#52c41a; --fa-secondary-opacity:1;height: 13px;"><g data-v-303bbf52="" class="fa-group"><path data-v-303bbf52="" fill="currentColor" d="M512 256a88 88 0 0 0-57.1-82.4A88 88 0 0 0 338.4 57.1a88 88 0 0 0-164.8 0A88 88 0 0 0 57.1 173.6a88 88 0 0 0 0 164.8 88 88 0 0 0 116.5 116.5 88 88 0 0 0 164.8 0 88 88 0 0 0 116.5-116.5A88 88 0 0 0 512 256zm-144.8-44.25l-131 130a11 11 0 0 1-15.55-.06l-75.72-76.33a11 11 0 0 1 .06-15.56L171 224a11 11 0 0 1 15.56.06l42.15 42.49 97.2-96.42a11 11 0 0 1 15.55.06l25.82 26a11 11 0 0 1-.08 15.56z" style="color:#52c41a"></path><path data-v-303bbf52="" fill="currentColor" d="M367.2 211.75l-131 130a11 11 0 0 1-15.55-.06l-75.72-76.33a11 11 0 0 1 .06-15.56L171 224a11 11 0 0 1 15.56.06l42.15 42.49 97.2-96.42a11 11 0 0 1 15.55.06l25.82 26a11 11 0 0 1-.06 15.56z" style="color:#fff"></path></g></svg>'
+			file.write('<td><a style="color:' + i[6][2] + '" href="https://www.luogu.com.cn/user/' + str(i[6][0]) + '">' + str(i[6][1]) + go + '</a></td>')
 			file.write('<td>' + i[7] + '</td><td>' + i[8] + '</td><td><span>' + i[9] + '</spane></td><td>' + GetTime(i[11]) + '</td>')
 			file.write('</tr>')
 	file.write('</tbody></table></div></div><div id="footer"><div class="container"><p>Powered By <a href="https://github.com/yzxoi/Submission-Monitor" target="_blank" rel="noopener noreferrer">yzxoi Submission Monitor</a></p></div></div></div><script src="https://cdn.jsdelivr.net/npm/mdui@1.0.2/dist/js/mdui.min.js"></script></body></html>')
